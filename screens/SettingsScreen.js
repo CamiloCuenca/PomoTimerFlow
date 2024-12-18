8f73baf44ec018b917ac4014c634a84e3a7c84c7
CamiloCuenca <camilocuenca1810@gmail.com>
2024-12-17 20:24:05 -0500

    feat: create button with icon component and JSON file to manage app colors

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,12 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native';
+import colors from '../constants/colors.json';
 
 
 export default function SettingsScreen(){
     return (
-        <View>
+        <View style={styles.container}>
             <Text>
                 Settings
             </Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: colors.secondary ,
+    },
+   
+});

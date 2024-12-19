d03038352f096bd3658298669d3ec1aa95185d43
CamiloCuenca <camilocuenca1810@gmail.com>
2024-12-18 20:46:11 -0500

    fix: adjust initial time to 25 minutes

--- screens/MainTimerScreen.js
+++ screens/MainTimerScreen.js
@@ -9,12 +9,12 @@ import Timer from '../components/Timer';
 
 export default function MainTimerScreen() {
   const [isPlaying, setIsPlaying] = useState(false); // Estado para iniciar/pausar el temporizador
-  const [time, setTime] = useState(1 * 60); // Tiempo inicial del temporizador (25 minutos en segundos)
+  const [time, setTime] = useState(25 * 60); // Tiempo inicial del temporizador (25 minutos en segundos)
 
   // Función para reiniciar el temporizador
   const resetTimer = () => {
     setIsPlaying(false); // Detener el temporizador
-    setTime(1 * 60); // Reiniciar el tiempo a 25 minutos
+    setTime(25 * 60); // Reiniciar el tiempo a 25 minutos
   };
 
   return (

17cb95be5974f1e8a4606a924b2bbbe796b23c1f
CamiloCuenca <camilocuenca1810@gmail.com>
2025-01-15 23:22:15 -0500

    add: implement stack navigation and create template for login #11

---
+++ screens/Login.js
@@ -0,0 +1,14 @@
+import { useForm,Controller } from "react-hook-form";
+import { View, Text , TextInput } from "react-native-web";
+
+
+export default function Login (){
+
+    return(
+
+    <View>
+        <Text>Login</Text>
+    </View>
+
+    )
+}

36b0ebbe340972a9396e59c78bef45f70f772968
CamiloCuenca <camilocuenca1810@gmail.com>
2026-01-05 19:00:23 -0500

    fix: implement font loading and splash screen handling in RootLayout #48

--- app/_layout.js
+++ app/_layout.js
@@ -5,9 +5,28 @@ import { AudioProvider } from '../context/AudioContext';
 import { AudioPlayerProvider } from '../context/AudioPlayerContext';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { TaskProvider } from '../context/TaskContext';
+import { useFonts } from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    ...Ionicons.font,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider>
       <AudioProvider>

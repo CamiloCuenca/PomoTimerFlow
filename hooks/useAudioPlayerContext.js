b91b064795b50309974d9cc9c546b3fe2e71accd
CamiloCuenca <camilocuenca1810@gmail.com>
2026-01-16 22:59:19 -0500

    fix: remove unused permissions and clean up package.json dependencies

--- hooks/useAudioPlayerContext.js
+++ hooks/useAudioPlayerContext.js
@@ -1,10 +1,1 @@
-import { useContext } from 'react';
-import { AudioPlayerContext } from '../context/AudioPlayerContext';
-
-export const useAudioPlayerContext = () => {
-  const context = useContext(AudioPlayerContext);
-  if (!context) {
-    throw new Error('useAudioPlayerContext debe usarse dentro de AudioPlayerProvider');
-  }
-  return context;
-};
+// Archivo eliminado: useAudioPlayerContext.js (audio)
